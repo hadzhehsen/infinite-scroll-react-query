7e7805176eb4c492c292916d6a93dd0c1493f9f9
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-24 13:58:03 +0300

    fix: build

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,6 +2,28 @@ import { PostPage } from '@/components/posts/post-page';
 import { getPersonalNews, getNewsPageContent } from '@/utils/get-post-query';
 import { notFound } from 'next/navigation';
 
+interface Post {
+  image: {
+    src: string;
+    width: number;
+    height: number;
+  };
+  title: string;
+  description: string;
+  date: Date;
+  source: string;
+  content_type: string;
+  url: string;
+}
+
+interface PersonalPost {
+  image: {
+    url: string;
+  };
+  title: string;
+  description: string;
+}
+
 export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 
@@ -10,15 +32,14 @@ export default async function Page({
 }: {
   params: { id: string };
 }) {
-  console.log({ id });
   const posts = await getNewsPageContent();
   const personalPosts = await getPersonalNews();
 
-  const post = posts.find((post, i) => {
+  const post = posts.find((post: Post, i: number) => {
     return `0${i}` === id;
   });
 
-  const personalPost = personalPosts.find((post, i) => {
+  const personalPost = personalPosts.find((post: PersonalPost, i: number) => {
     return post.title === `news${i + 1}`;
   });
 
