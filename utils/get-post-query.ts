f7ab87e23092e69c41e1516b1726e4d1b1d06864
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-24 13:41:48 +0300

    feat: add personal posts func

--- utils/get-post-query.ts
+++ utils/get-post-query.ts
@@ -1,17 +1,24 @@
-import { cache } from "react";
+import { cache } from 'react';
+import { storyblokInit, apiPlugin } from '@storyblok/js';
+import {
+  REVALIDATE_TIME,
+  STORYBLOK_ACCESS_TOKEN,
+  VERCEL_ENV,
+} from '../constants';
 
-const FALCONER_ENDPOINT = "https://falconer.haqq.sh";
+export const revalidate = REVALIDATE_TIME;
+const FALCONER_ENDPOINT = 'https://falconer.haqq.sh';
 
 export const getNewsPageContent = cache(async (page = 0, limit = 20) => {
   try {
-    const requestURL = new URL("/islamic/news", FALCONER_ENDPOINT);
-    requestURL.searchParams.append("page", page.toString());
-    requestURL.searchParams.append("limit", limit.toString());
+    const requestURL = new URL('/islamic/news', FALCONER_ENDPOINT);
+    requestURL.searchParams.append('page', page.toString());
+    requestURL.searchParams.append('limit', limit.toString());
 
     const response = await fetch(requestURL, {
-      method: "get",
+      method: 'get',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       next: {
         revalidate: 180,
@@ -27,4 +34,23 @@ export const getNewsPageContent = cache(async (page = 0, limit = 20) => {
     console.error(error);
     return;
   }
+});
+
+export const getPersonalNews = cache(async (page = 0, limit = 3) => {
+  const { storyblokApi } = storyblokInit({
+    accessToken: STORYBLOK_ACCESS_TOKEN,
+    use: [apiPlugin],
+  });
+
+  if (!storyblokApi) {
+    throw new Error('Failed to init storyblok');
+  }
+
+  const response = await storyblokApi.get('cdn/stories/news', {
+    version: VERCEL_ENV === 'production' ? 'published' : 'draft',
+    // page,
+    // per_page: limit,
+  });
+
+  return response.data.story.content.columns[0].columns;
 });
