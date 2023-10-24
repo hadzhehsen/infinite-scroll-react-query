56dec11af20c83497ce82081829ce57b0c643019
Jihad Hadj Husein <skiptomylou999@gmail.com>
2023-10-24 13:42:18 +0300

    fix: constants

--- constants.ts
+++ constants.ts
@@ -1,1 +1,5 @@
 export const NEWS_LIMIT = 50;
+export const REVALIDATE_TIME = 180;
+export const STORYBLOK_ACCESS_TOKEN =
+  process.env['PERSONAL_STORYBLOK_ACCESS_TOKEN'];
+export const VERCEL_ENV = process.env['VERCEL_ENV'];
